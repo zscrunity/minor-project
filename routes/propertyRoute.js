53a82d714223720821868e59cd471366a0f71889
zscrunity <zscrunity@gmail.com>
2023-02-01 20:08:01 +0545

    Second Commit

--- routes/propertyRoute.js
+++ routes/propertyRoute.js
@@ -1,16 +1,78 @@
 const express = require("express");
 const router = express.Router();
 const Property = require("../models/property");
+const { verifyTokenAndAdmin } = require("./verifyToken");
 
-router.post("/propertyadd", async ( req , res ) => {
+//CREATE
+router.post("/propertyadd", verifyTokenAndAdmin, async ( req , res ) => {
   const newProperty = new Property(req.body);  
   try{
     const property = await newProperty.save();
-    res.status(201).json(property);
+    res.status(200).json(property);
    
   } catch (error) {
     return res.status(500).json(error);
   }
 }); 
+
+
+//UPDATE
+router.put("/:id", verifyTokenAndAdmin, async(req,res) => {
+  try {
+    const updatedProperty = await Property.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedProperty);
+  } catch(err) {
+    res.status(500).json.apply(err);
+  }
+})
+
+//DELETE
+router.delete("/:id", verifyTokenAndAdmin, async (req,res) => {
+  try {
+    await Property.findByIdAndDelete(req.params.id);
+    res.status(200).json("Property has been deleted!!!");
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET PROPERTY
+router.get("/find/:id", async(req,res) => {
+  try {
+    const property = await Property.findById(req.params.id);
+    res.status(200).json(property);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL PROPERTY
+router.get("/", async (req,res) => {
+  const qNew = req.query.new
+  const qCategory = req.query.category
+  try {
+    let properties;
+    
+    if(qNew) {
+      properties = await Property.find().sort({createdAt: -1}).limit(5)
+    } else if(qCategory) {
+      properties = await Property.find({category: {
+        $in: [qCategory],
+      },
+    });
+    } else {
+      properties = await Property.find();
+    }
+    res.status(200).json(properties);
+  } catch {
+
+  }
+});
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 const userModel = require("../models/user");
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
 
 const router = require("express").Router();
 
+
+//UPDATE
 router.put("/:id", verifyTokenAndAuthorization, async (req,res) => {
   if(req.body.password) {
     req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString();
@@ -20,7 +22,43 @@ router.put("/:id", verifyTokenAndAuthorization, async (req,res) => {
   } catch(err) { 
     res.status(500).json(err);
   }
-  
-})
+});
+
+//DELETE
+router.delete("/:id",verifyTokenAndAuthorization , async (req,res) => {
+  try {
+    await userModel.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted!!!");
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET USER
+router.delete("/find/:id",verifyTokenAndAdmin , async (req,res) => {
+  try {
+    const user = await userModel.findById(req.params.id);
+    
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL USER
+router.delete("/",verifyTokenAndAdmin , async (req,res) => {
+  const query = req.query.new;
+
+  try {
+    const users = query ? await userModel.find().sort({_id: -1}).limit(1) : await User.find();
+    res.status(200).json(users);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
 
 module.exports = router;
